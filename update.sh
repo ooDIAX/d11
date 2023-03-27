b83fb0c0a4fc922a21f523f3bbec3b418adca1fd
niko <sharikadze.nikoloz03@gmail.com>
2023-03-27 11:51:35 +0700

    added update.sh, modified CI

---
+++ update.sh
@@ -0,0 +1,10 @@
+sudo pkill gunicorn
+docker compose down
+docker compose up -d --build
+pip install -r requirements.txt
+echo "Sleeping for 10 seconds to allow the database to start up..."
+sleep 10
+python3 test-db.py
+/home/ubuntu/.local/bin/gunicorn -b 0.0.0.0:8888 mini-server:app --daemon
+sleep 5
+tail -n 100 logs/webserver.log
