fcf231acb4d11f23e86aac7437765281d31761e0
niko <sharikadze.nikoloz03@gmail.com>
2023-03-27 12:14:44 +0700

    removed tail

--- update.sh
+++ update.sh
@@ -6,5 +6,5 @@ echo "Sleeping for 10 seconds to allow the database to start up..."
 sleep 10
 python3 test-db.py
 /home/ubuntu/.local/bin/gunicorn -b 0.0.0.0:8888 mini-server:app --daemon
-sleep 5
-tail -n 100 logs/webserver.log
+# sleep 5
+# tail -n 100 logs/webserver.log
